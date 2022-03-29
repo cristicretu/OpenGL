d5552e5abab359b50fdb78fc6a5ab956df719ffe
Cristian Crețu <crisemcr@gmail.com>
2022-03-29 20:14:33 +0300

    [feat]: debug and build works

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main ()
 {
-    std::cout << "Hello World!" << std::endl;
-    return EXIT_SUCCESS;
+  const std::string greeting = "Hello, World!";
+  
+  for (auto c : greeting)
+    std::cout << c << std::endl;
+
+  std::cout << greeting << std::endl;
+  return EXIT_SUCCESS;
 }
